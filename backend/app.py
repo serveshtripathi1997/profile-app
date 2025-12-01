78d57fb0df04dd3dfb6a0d8575dc95dcef1486d1
Servesh Tripathi <Servesh.Tripathi@teknopoint.local>
2025-12-01 11:29:08 +0530

    first

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,23 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 import os
 
-app = Flask(__name__, static_folder="../frontend", static_url_path="")
+# Backend directory = /Python/backend
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# ---------- API ----------
+# Frontend directory = /Python/frontend
+FRONTEND_FOLDER = os.path.join(BASE_DIR, "..", "frontend")
+
+# Media directory = /Python/backend/Media
+MEDIA_FOLDER = os.path.join(BASE_DIR, "Media")
+
+app = Flask(
+    __name__,
+    static_folder=FRONTEND_FOLDER,     # Serve CSS/JS
+    static_url_path=""
+)
+
+
+# ------------ API ------------
 @app.route("/profile")
 def profile():
     return jsonify({
@@ -12,16 +26,19 @@ def profile():
         "release_date": "01.12.2025"
     })
 
-# ---------- SERVE IMAGES ----------
-@app.route('/media/<path:filename>')
-def media(filename):
-    media_folder = os.path.join(os.getcwd(), "Media")
-    return send_from_directory(media_folder, filename)
 
-# ---------- SERVE FRONTEND ----------
+# ---------- Serve Media ----------
+@app.route("/media/<path:filename>")
+def media_files(filename):
+    return send_from_directory(MEDIA_FOLDER, filename)
+
+
+# ---------- Serve Frontend ----------
 @app.route("/")
 def home():
-    return send_from_directory("../frontend", "index.html")
+    return send_from_directory(FRONTEND_FOLDER, "index.html")
 
+
+# ---------- Run Server ----------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9000, debug=True)
